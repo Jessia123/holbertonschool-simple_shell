8d7ca03856535d6311a956111bbde08da77450d6
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-04-24 11:04:19 -0500

    adding changes

--- _shell.c
+++ _shell.c
@@ -37,17 +37,19 @@ int main()
 		exit(0);
 		}
 	cmdline[strlen(cmdline)-1] = '\0';
+   for (;;)
+	 {
+   char *argv[2];
+   argv [0] = "/bin/sh";
+	 argv [1] = NULL;
+   execvp(argv[0], argv);
+   }
 
 	}
 
 	free(cmdline);
 	return (0);
 
-	for (;;)
-{
-char *argv[2];
-argv [0] = "/bin/sh";
-argv [1] = NULL;
-execvp(argv[0], argv);
-}
+	
+
 }
