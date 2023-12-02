b4971f9fa0b4d22d60599ed1120f7f34f3eeff87
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-02 06:11:18 +0000

    shell 1.0

--- shell.c
+++ shell.c
@@ -4,6 +4,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+int main(int argc, char *argv[]) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+    char *args[2];
+
+    if (argc > 1) {
+        args[0] = argv[1];
+        args[1] = NULL;
+        pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            execv(args[0], args);
+
+            /* If we get here, execv failed */
+            perror(args[0]);
+            exit(EXIT_FAILURE);
+        } else {
+            wait(NULL);
+        }
+
+        return 0;
+    }
+
 int main() {
     char *line = NULL;
     size_t len = 0;
