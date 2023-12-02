a1455ce2a66fb1d1e9489c7651de0469797d10e4
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-02 05:28:12 +0000

    add line

--- AUTHORS.sh
+++ AUTHORS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "This file lists all the contributors to this repository."
 
 echo "Author 1: Jessia Grimes - jessia.grimes@example.com"
 echo "Author 2: Jane Doe - jane.doe@example.com"
