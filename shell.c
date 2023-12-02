86a71dc36bada176bbba44defae3505566df6b02
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-02 06:31:44 +0000

    betty styled

--- shell.c
+++ shell.c
@@ -1,47 +1,40 @@
-/**
- * @file shell.c
- * @brief A simple shell program that executes a command passed as a command line argument.
- *
- * This program takes a command as a command line argument and executes it using the `execv` function.
- * It forks a child process, and the child process uses `execv` to execute the command.
- * The parent process waits for the child process to complete using the `wait` function.
- *
- * @param argc The number of command line arguments.
- * @param argv An array of strings containing the command line arguments.
- * @return 0 on success, -1 on failure.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include<shell.h>
+#include "shell.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     pid_t pid;
     char *args[2];
 
-    if (argc > 1) {
+    if (argc > 1)
+    {
         args[0] = argv[1];
         args[1] = NULL;
         pid = fork();
 
-        if (pid == -1) {
+        if (pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0) {
+        if (pid == 0)
+        {
             execv(args[0], args);
 
             /* If we get here, execv failed */
             perror(args[0]);
             exit(EXIT_FAILURE);
-        } else {
+        }
+        else
+        {
             wait(NULL);
         }
 
@@ -49,18 +42,6 @@ int main(int argc, char *argv[]) {
     }
 }
 
-
-
-/**
- * main - Entry point for the shell program
- *
- * Description:
- * This function serves as the entry point for the shell program.
- * It initializes the shell, reads user input, and executes commands accordingly.
- *
- * Return:
- * 0 on success, -1 on failure
- */
 int main(void)
 {
     char *line = NULL;
@@ -69,15 +50,20 @@ int main(void)
     pid_t pid;
     char *args[2];
 
-    while (1) {
+    while (1)
+    {
         printf("> ");
         read = getline(&line, &len, stdin);
 
-        if (read == -1) {
-            if (feof(stdin)) {
+        if (read == -1)
+        {
+            if (feof(stdin))
+            {
                 printf("\n");
                 exit(EXIT_SUCCESS);
-            } else {
+            }
+            else
+            {
                 perror("getline");
                 exit(EXIT_FAILURE);
             }
@@ -87,7 +73,8 @@ int main(void)
         line[strcspn(line, "\n")] = 0;
 
         /* Skip empty lines */
-        if (strlen(line) == 0) {
+        if (strlen(line) == 0)
+        {
             continue;
         }
 
@@ -95,9 +82,13 @@ int main(void)
         args[1] = NULL;
         pid = fork();
 
-        if (pid == -1) {
+        if (pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
+        }
+    }
+}
         }
 
         if (pid == 0) {
