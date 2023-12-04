3419d111cd35bea9099815684db8933e881ce4a0
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-04 13:02:23 +0000

    deleted and start over

--- main.c
+++ main.c
@@ -1,64 +1,16 @@
+#include "main.h"
+
 /**
- * @file main.c
- * @brief A simple shell program that takes user commands and executes them.
- * The program displays a prompt, reads the user's command, and executes it using fork and exec.
- * The parent process waits for the child process to complete before displaying the next prompt.
- * The program continues to run until the user enters Ctrl+D (end of file).
- */
+  * main - Main entry point for our program
+  * @argc: Argument count to the main
+  * @argv: Pointer to array of argument values
+  *
+  * Return: O Always success
+  */
 
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
-#define MAX_COMMAND_LENGTH 100
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    char command[MAX_COMMAND_LENGTH];
-    int status;
-
-    while (1)
-    {
-        printf("$ "); // Displaying the prompt
-
-        if (fgets(command, sizeof(command), stdin) == NULL)
-        {
-            if (feof(stdin))
-            {
-                printf("\n"); // Print newline for better formatting
-                break; // Exit the loop on Ctrl+D (end of file)
-            }
-        }
-
-        // Removing the newline character from the command
-        command[strcspn(command, "\n")] = '\0';
-
-        pid_t pid = fork();
-
-        if (pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            // Child process executes the command
-            if (execlp(command, command, NULL) == -1)
-            {
-                // If execution fails, print error and exit child process
-                perror("execlp");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            // Parent process waits for the child to complete
-            waitpid(pid, &status, 0);
-        }
-    }
-
-    return 0;
+        (void)argc, (void)argv;
+        write(STDOUT_FILENO, "MyShell$ ", 9);
+        return (0);
 }
