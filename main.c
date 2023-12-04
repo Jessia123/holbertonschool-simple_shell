2b7f5768ac282b2c0bb688d3110450a0669c33d4
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-04 13:10:35 +0000

    fix

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int main(int argc, char **argv)
 {
-        (void)argc, (void)argv;
-        char *buf = NULL;
-        size_t count = 0;
-        ssize_t nread;
+    (void)argc, (void)argv;
+    char *buf = NULL;
+    size_t count = 0;
+    ssize_t nread;
 
-        write(STDOUT_FILENO, "MyShell$ ", 9);
-        nread = getline(&buf, &count, stdin);
+    write(STDOUT_FILENO, "MyShell$ ", 9);
+    nread = getline(&buf, &count, stdin);
 
-        if (nread ==  -1)
-        {
-                 perror("Exiting shell");
-                 exit(1);
-        }
-        printf("%s", buf);
-        free(buf);
-        return (0);
+    if (nread ==  -1)
+    {
+         perror("Exiting shell");
+         exit(1);
+    }
+    printf("%s", buf);
+    free(buf);
+    return (0);
 }
