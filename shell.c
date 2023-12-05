7745ca22353d6feaaeb798adb91095f69a1020d8
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-05 02:43:42 +0000

    fix doxumentation

--- shell.c
+++ shell.c
@@ -13,7 +13,7 @@ int main(void) {
     char *token;
     char *args[MAX_ARGS];
     int i;
-    pid_t pid; // Declare pid at the start of the block
+    pid_t pid; /* Declare pid at the start of the block */
 
     while (1) {
         /* Displaying the prompt */
