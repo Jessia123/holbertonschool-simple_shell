b0e536856f391ad736c9a8b1a9eccc58895ac4eb
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-05 21:55:27 +0000

    documentation

--- main_shell.c
+++ main_shell.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Description: This function creates a simple shell that continuously prompts
+ * the user for commands, reads the input, and executes commands until the
+ * user exits.
+ *
+ * Return: Always returns 0.
+ */
 int main(void)
 {
 	char *line;
