4956ce3b8fd7e3176c907009dfbcf900137751db
popstarmav <cj.johnson2017@gmail.com>
2023-12-05 20:00:15 +0000

    shell beta

--- AUTHORS.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "This file lists all the contributors to this repository."
-
-echo "Author 1: Jessia Grimes - jessia.grimes@example.com"
-echo "Author 2: Jane Doe - jane.doe@example.com"
---
+++ shell_func.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * read_line - Read a line of input from the user.
+ *
+ * Return: A pointer to the input line.
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufszie = 0;
+	getline(&line, &bufszie, stdin);
+	return (line);
+}
+/**
+ * split_line - Split a string into tokens.
+ * @line: The input string to split.
+ *
+ * Return: An array of pointers to the tokens.
+ */
+char **split_line(char *line)
+{
+	int bufsize = BUFFER_SIZE, positon = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\n");
+	while (token != NULL)
+	{
+		tokens[positon] = token;
+		positon++;
+
+		if (positon >= bufsize)
+		{
+			bufsize += BUFFER_SIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "Allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, " \t\n");
+	}
+	tokens[positon] = NULL;
+	return (tokens);
+}
+/**
+ * execute - Execute a command.
+ * @args: An array of arguments, including the command itself.
+ *
+ * Return: 1 if the shell should continue, 0 if it should exit.
+ */
+int execute(char **args)
+{
+	pid_t pid;
+	pid = fork();
+
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("Error");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if(pid < 0)
+	{
+		perror("Error");
+	}
+	else
+	{
+		wait(NULL);
+	}
+	return (1);
+}
+
