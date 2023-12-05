9ae5f4f964aa2cc9480579dad0bb70c2c46d0bb8
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-05 02:51:09 +0000

    fix

--- shell.c
+++ shell.c
@@ -5,7 +5,15 @@
 #include <sys/wait.h>
 
 #define MAX_ARGS 100 /* Maximum number of arguments */
-
+/**
+ * main - Entry point for the shell program
+ *
+ * Description: Implements a simple shell that reads commands
+ *              from the user, tokenizes them, and executes
+ *              using the execve system call in child processes.
+ *
+ * Return: Always returns 0 on successful completion.
+ */
 int main(void) {
     char *buffer = NULL;
     size_t bufsize = 0;
