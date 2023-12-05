bbc78aeaad62cdef26e252f727a1874b12b160e7
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-12-05 02:36:44 +0000

    fix execve

--- shell.c
+++ shell.c
@@ -6,39 +6,25 @@
 
 #define MAX_ARGS 100 /* Maximum number of arguments */
 
-/**
- * main - Simple Shell
- * Description: Basic UNIX command-line interpreter
- * Return: Always 0
- */
-int main(void)
-{
+int main(void) {
     char *buffer = NULL;
     size_t bufsize = 0;
     ssize_t characters;
-
     char *token;
     char *args[MAX_ARGS];
     int i;
 
-    pid_t pid; /* Declare pid outside the loop */
-
-    while (1)
-    {
+    while (1) {
         /* Displaying the prompt */
         printf("$ ");
         characters = getline(&buffer, &bufsize, stdin);
 
         /* Error handling for getline */
-        if (characters == -1)
-        {
-            if (feof(stdin))
-            {
+        if (characters == -1) {
+            if (feof(stdin)) {
                 printf("\n");
                 break; /* Exit loop on Ctrl+D (end of file) */
-            }
-            else
-            {
+            } else {
                 perror("getline");
                 exit(EXIT_FAILURE);
             }
@@ -48,36 +34,30 @@ int main(void)
         buffer[strcspn(buffer, "\n")] = '\0';
 
         token = strtok(buffer, " ");
-        i = 0; /* Reset i for each loop iteration */
+        i = 0; // Reset i for each loop iteration
 
         /* Tokenize input into command and arguments */
-        while (token != NULL)
-        {
+        while (token != NULL) {
             args[i++] = token;
             token = strtok(NULL, " ");
         }
         args[i] = NULL; /* Set the last element to NULL for execve */
 
-        pid = fork(); /* Assign fork result to pid */
+        pid_t pid = fork();
 
         /* Error handling for fork */
-        if (pid == -1)
-        {
+        if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
         /* Child process executes the command */
-        if (pid == 0)
-        {
-            if (execve(args[0], args, NULL) == -1)
-            {
+        if (pid == 0) {
+            if (execve(args[0], args, NULL) == -1) {
                 perror("execve");
                 exit(EXIT_FAILURE);
             }
-        }
-        else /* Parent process waits for child */
-        {
+        } else { /* Parent process waits for child */
             int status;
             waitpid(pid, &status, 0);
         }
